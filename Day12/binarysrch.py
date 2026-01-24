1deb7b45522ded715a77dc6d26cc93a5a5791e50
rites-h14 <amateurhuman14@gmail.com>
2026-01-24 10:24:10 +0530

    completed Day 12 problem

---
+++ Day12/binarysrch.py
@@ -0,0 +1,42 @@
+class Solution(object):
+    def searchRange(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+
+        def find_first(nums, target):
+            left, right = 0, len(nums) - 1
+            first = -1
+            while left <= right:
+                mid = (left + right) // 2
+                if nums[mid] == target:
+                    first = mid
+                    right = mid - 1
+                elif nums[mid] < target:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+            return first
+
+        def find_last(nums, target):
+            left, right = 0, len(nums) - 1
+            last = -1
+            while left <= right:
+                mid = (left + right) // 2
+                if nums[mid] == target:
+                    last = mid
+                    left = mid + 1
+                elif nums[mid] < target:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+            return last
+
+        first = find_first(nums, target)
+        if first == -1:
+            return [-1, -1]
+
+        last = find_last(nums, target)
+        return [first, last]
